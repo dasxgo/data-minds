96748f29bd3100ec59edb345ff797d7c61e23412
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-19 11:39:25 -0500

    update

---
+++ src/01-premier-league/04-run.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import main
+from utils import df_results, df_stats
+from main import dfy
+
+main.main()
+
+print('='*120)
+print('='*120)
+
+print(main.dfy)
+
+
--- src/01-premier-league/main.py
+++ src/01-premier-league/main.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 from utils import df_results, df_stats
 
+dfy = (df_results[['home_goals', 'away_goals']])
+print(dfy)
+
 def main():
-    print(df_results[['home_goals', 'away_goals']])
+    dfy = (df_results[['home_goals', 'away_goals']])
+    print(dfy)
     print('='*120)
     print(df_stats[0:7])
     
