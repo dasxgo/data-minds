ab7497f6bb8422ef6696ec76dbd7785125a52049
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-19 14:00:20 -0500

    update

--- src/02-pass/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np 
-
-class Home:
-    pass
-
-data = [1,2,3,4]
---
+++ src/pass/modulo.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np 
+import sys
+sys.path.append("/home/dasxgo/dev/jupyter/src")
+from premier import utils
+
+utils.df_stats
