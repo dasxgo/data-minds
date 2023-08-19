e7c14c93b295e1ee0d84f2662f71074a51465a1e
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-19 11:49:08 -0500

    update

--- src/01-premier-league/run.py
+++ src/01-premier-league/run.py
@@ -5,10 +5,14 @@ import main
 from utils import df_results, df_stats
 from main import dfy
 
+# Call everything contained in the main file as run function
+
 main.run()
 
 print('='*120)
 print('='*120)
+
+# I can call a part of the code
 
 print(main.dfy)
 
