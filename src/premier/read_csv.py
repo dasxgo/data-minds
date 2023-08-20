00547d1704158b334e040d22c54687237e580ee7
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-20 14:27:11 -0500

    update

--- src/premier/read_csv.py
+++ src/premier/read_csv.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 
 df_results = pd.read_csv('/home/dasxgo/dev/jupyter/data/results.csv', sep=',', header=0)  
-print(df_results)
-
-print('='*120)
-
 df_stats = pd.read_csv('/home/dasxgo/dev/jupyter/data/stats.csv', sep=',', header=0)
-print(df_stats)
+
+
+if __name__ == '__main__':
+    print(df_results.head())
+    print('='*120)
+    print(df_stats.head())
 
 
 
