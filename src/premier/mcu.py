46c132cbb9dfaa4cd2f080afb22787d3a6bf6c1e
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-21 19:37:18 -0500

    update

--- src/premier/mcu.py
+++ src/premier/mcu.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from utils import df_results, df_stats
+from read_csv import df_results, df_stats
 
 if __name__ == '__main__':
     mudf = df_stats['team'] == "Manchester United"
