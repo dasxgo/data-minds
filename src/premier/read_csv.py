a58b0ab220712f71cf9efe54930a3c8cda5fa473
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-22 16:36:18 -0500

    updatte

--- src/premier/read_csv.py
+++ src/premier/read_csv.py
@@ -6,10 +6,38 @@ df_stats = pd.read_csv('/home/dasxgo/dev/data-insights/data/stats.csv', sep=',',
 
 
 if __name__ == '__main__':
+    # Results data
     print(df_results.head())
+    print('-'*120)
+    print(f'results shape => ', df_results.shape)
+    print('-'*120)
+    print(f'results index => ', df_results.index)
+    print('-'*120)
+    print(f'results columns => ', df_results.columns)
+    print('-'*120)
+    print(df_results.dtypes)
+    print('-'*120)
+    print(df_results.isnull().sum())
+    
     print('='*120)
-    print(df_stats.head())
 
+    
+    # stats data
+    print(df_stats.head())
+    print('-'*120)
+    print(f'stats shape => ', df_stats.shape)
+    print('-'*120)
+    print(f'stats index => ', df_stats.index)
+    print('-'*120)
+    print(f' stats columns => ', df_stats.columns)
+    print('-'*120)
+    print(df_stats.dtypes)
+    print('-'*120)
+    print(df_stats.isnull().sum())
+    
+    print('='*120)
+
+    # Other data
     print(df_results[['home_goals', 'away_goals']])
     print('='*120)
     print(df_stats[0:7])
--- src/premier/utils.py
+++ src/premier/utils.py
@@ -2,11 +2,24 @@ import pandas as pd
 import numpy as np
 from read_csv import df_results, df_stats
 
+# Manchester United
 
 mudf = df_stats['team'] == "Manchester United"
 mudf = df_stats[mudf]
 
+season = mudf.groupby('season').sum()
+
+season_2017_2018 = season.loc['2017-2018']
+
 season_goals = df_results.groupby('season').sum(['home_goals', 'away_goals'])
 
 if __name__ == '__main__':
+    print(season)
+    print('-'*120)
     print(season_goals)
+    print('-'*120)
+    print(f' minimun goals season Manchester United => ', season['goals'].min())
+    print('-'*120)
+    print(season_2017_2018)
+    print('-'*120)
+   
