c8d9f066f1dece6b1c6760d9e1d3332d7154f19c
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-22 19:25:28 -0500

    update

--- src/premier/charts.py
+++ src/premier/charts.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import utils
 import read_csv
 
-def plot_season_goals(data):
+def plot_goals_home(data):
     plt.xticks(rotation=90)
     sns.lineplot(x="season", y="home_goals", data=data)
     plt.show()
@@ -23,18 +23,18 @@ def plot_goals(data):
     sns.barplot(x="season",y="goals",data=data)
     plt.show()
 
-    plt.figure(figsize=(15, 12))
+    plt.figure(figsize=(10, 6))
     plt.style.use('seaborn-poster')
     plt.xticks(rotation=90);
     sns.barplot(x="goals",y="team",data=data)
     plt.show()
 
 if __name__ == '__main__':
-    data_season_goals = utils.season_goals
+    data_goals_home = utils.season_goals
     data_mudf = utils.mudf
     data_goals = read_csv.df_stats
     
-    plot_season_goals(data_season_goals)
+    plot_goals_home(data_goals_home)
     plot_mudf(data_mudf)
     plot_goals(data_goals)
 
