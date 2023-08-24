6b7fc49e7a98dc7dc29e26d854419fb130ddfc6e
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-24 16:36:52 -0500

    update

--- src/premier/main.py
+++ src/premier/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import load
 import utils
 
-
 def main():
 
     df = load.df_stats
@@ -33,6 +32,20 @@ def main():
     df['total_matches'] = df['wins'] + df['losses'] + df['draws']
     
     # Changing the column location
+    pop_column2 = df.pop('total_matches')
+    df.insert(4, 'total_matches', pop_column2)
+    print(df.head())
+
+    print('-'*120)
+
+    # Creating a new column draw
+    df['draws'] = 38 - df['wins'] - df['losses']
+    # Changing the column location
+    pop_column = df.pop('draws')
+    df.insert(3, 'draws', pop_column)
+    # Creating a new column total_matches
+    df['total_matches'] = df['wins'] + df['losses'] + df['draws']
+    #Changing the column location
     pop_column2 = df.pop('total_matches')
     df.insert(4, 'total_matches', pop_column2)
     print(df.head())
