d73e304cca74bd67eac5352c6d1f7557ff8a4728
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-05 19:37:56 -0500

    update

--- src/bike/main.py
+++ src/bike/main.py
@@ -2,5 +2,53 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import load
 
-pass
+df = load.data
+
+def main():
+    # Remaning some of columns
+    df.rename(columns={'cnt': 'count', 't1' : 'c_temp', 't2' : 'f_temp', 'hum' : 'humidity'}, inplace= True)
+    print(df.columns)
+    print('-'*120)
+
+    # Datatypes of each columns
+    print(df.dtypes)
+    print('-'*120)
+
+    # Missing value
+    print(df.isnull().sum())
+    print('-'*120)
+
+    # Save the DataFrame to a CSV file
+
+    route = '/home/dasxgo/dev/data-minds/reports/01-bike.csv'
+    df.to_csv(route, index=False)  
+    print(f'DataFrame save in {route}')
+    print('-'*120)
+
+    # Feature Engineering 4 new columns from timestamp column and also weather_code
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    df['year'] = df['timestamp'].dt.year
+    df['day'] = df['timestamp'].dt.day_name()
+    df['hour'] = df['timestamp'].dt.hour
+    df['week'] = df['timestamp'].dt.isocalendar().week
+
+    weather_dict = {1: 'clear', 2: 'Scattered Clouds', 3: 'Broken Clouds',
+                     4: 'Cloudy', 7: 'Rain', 10: 'Rain with Thunderstorm',
+                     26: 'Showfall'}
+    df['wheather'] = df['weather_code'].map(weather_dict)
+
+    # Daily average bike shares per season
+
+    grouped_season_avg = ((df.groupby(['season'])['count'].mean())*24).reset_index()
+    print(grouped_season_avg)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
