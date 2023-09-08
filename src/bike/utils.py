cb1a08f90ca54ee19e896d89cf04b3020a7fa733
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-08 06:24:43 -0500

    utils upload code

--- src/bike/utils.py
+++ src/bike/utils.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import numpy as np
+import load
 
-pass
+df = load.data
+
+def categorize_stations(df):
+    spring = df[df['season']==0]
+    autumn = df[df['season']==2]
+    winter = df[df['season']==3]
+    summer = df[df['season']==1]
+    return spring, autumn, winter, summer
+
+if __name__ == '__main__':
+    spring_df, autumn_df, winter_df, summer_df = categorize_stations(df)
+
+
