9f51a185af3c7fc6518a246522ab6fb7af45a1ab
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-10 08:10:57 -0500

    update

--- src/bike/main.py
+++ src/bike/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import load
+from utils import categorize_stations
 
 df = load.data
 
@@ -49,14 +50,20 @@ def main():
     winter = df[df['season']==3]
     summer = df[df['season']==1]
 
+    spring_df, autumn_df, winter_df, summer_df = categorize_stations(df)
+
     # Lowest number of bikes shares in winter
 
     print(winter[winter['count']==winter['count'].min()])
+    print('-'*120)
+    print(f'Minimum winter count =>', winter_df['count'].min())
     print('-'*120)
 
     # Highest number of bike share in autumn or fall
 
     print(autumn[autumn['count']==autumn['count'].max()])
+    print('-'*120)
+    print(f' Maximun autumn count =>',autumn_df['count'].max())
     print('-'*120)
 
     # How is the bike rental demand distributed over weeks 
