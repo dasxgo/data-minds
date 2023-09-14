3b683f85c4ebfd43795c7c9c47fe690f3fd40962
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-14 18:40:12 -0500

    update charts

--- src/bike/charts.py
+++ src/bike/charts.py
@@ -12,7 +12,6 @@ print(df.shape)
 if __name__ == '__main__':
     # Are there any specific time periods with higher/lower bike rental counts?
     bike_count_avg = df.groupby(['hour'])['count'].mean().reset_index(name='avg_rides')
-
     plt.figure(figsize=(14,8))
     plt.barh(y=bike_count_avg['hour'], width=bike_count_avg['avg_rides'])
     plt.xlabel('Average bike rental count')
@@ -26,7 +25,7 @@ if __name__ == '__main__':
     sns.set_style('whitegrid')
     plt.figure(figsize=(12,6))
     explode = (0.1, 0.1, 0.1, 0.1)
-    plt.pie(grouped_seasons['ride_count'], labels = ['spring', 'Summer', 'Autumn', 'Winter'], 
+    plt.pie(grouped_seasons['ride_count'], labels = ['Spring', 'Summer', 'Autumn', 'Winter'], 
             explode=explode, autopct='%1.1f%%', shadow=True, startangle=90)
     plt.title('Bike sharing w.r.t Seasons')
     plt.show()
