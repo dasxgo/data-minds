ed53f720dc4c1ddb1e35cbd9c92e3fdc3658f425
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-14 06:32:24 -0500

    update charts

--- src/bike/charts.py
+++ src/bike/charts.py
@@ -3,19 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 df = pd.read_csv('/home/dasxgo/dev/data-minds/reports/02-bike.csv')
 print(df.head())
 print(df.shape)
 
-# Are there any specific time periods with higher/lower bike rental counts?
 
-bike_count_avg = df.groupby(['hour'])['count'].mean().reset_index(name='avg_rides')
+if __name__ == '__main__':
+    # Are there any specific time periods with higher/lower bike rental counts?
+    bike_count_avg = df.groupby(['hour'])['count'].mean().reset_index(name='avg_rides')
 
-plt.figure(figsize=(14,8))
-plt.barh(y=bike_count_avg['hour'], width=bike_count_avg['avg_rides'])
-plt.xlabel('Average bike rental count')
-plt.ylabel('Hour of day')
-plt.title('Hourly Bike rental counts')
-plt.show()
-
+    plt.figure(figsize=(14,8))
+    plt.barh(y=bike_count_avg['hour'], width=bike_count_avg['avg_rides'])
+    plt.xlabel('Average bike rental count')
+    plt.ylabel('Hour of day')
+    plt.title('Hourly Bike rental counts')
+    plt.show()
 
