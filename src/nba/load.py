15f5b2116c7bc03e701de975c450fd0aa1dc8447
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-09-17 18:56:37 -0500

    upload data

--- src/nba/load.py
+++ src/nba/load.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import numpy as np
 
-pass
+data = pd.read_csv('/home/dasxgo/dev/data-minds/data/all_seasons.csv')
+
+if __name__ == '__main__':
+    print(data.head())
+    print('-'*120)
+    print(data.shape)
+    print('-'*120)
+    print(data.tail())
